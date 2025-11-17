5a4564c297c9975ccad1b4c2f26b99b62c0003f5
Shyamal Suhana Chandra <shyamal@research.edu>
2025-11-17 15:16:32 -0600

    Add Pages enablement helper script

---
+++ docs/enable-pages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Copyright (C) 2025, Shyamal Suhana Chandra
+echo "GitHub Pages Setup:"
+echo "Visit: https://github.com/Sapana-Micro-Software/progressive-learning-chess-engine/settings/pages"
+echo "Set Source to 'GitHub Actions' and click Save"
